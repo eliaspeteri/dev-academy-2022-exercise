42d3ee24da6f79ae481f0bd4f983aa2ff1ea925f
Elias Peteri <elias.peteri@hotmail.com>
2021-12-13 19:26:58 +0200

    server now defaults to port 8080

--- server/index.ts
+++ server/index.ts
@@ -3,14 +3,14 @@ import { createServer, Server } from 'http';
 import Config from './utils/config';
 import logger from './utils/logger';
 import app from './app';
-logger.info(`connecting to ${Config.MONGODB_URI}`);
 
 const server: Server = createServer(app);
 
 try {
-  server.listen(Config.PORT, (): void => {
+  server.listen(Config.PORT || 8080, (): void => {
     logger.info(`Connected successfully on port ${Config.PORT}`);
   });
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
 } catch (error: any) {
   logger.error(error.message);
 }
